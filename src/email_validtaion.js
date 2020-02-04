8667422413b09aa988d2f13358da62217ea11ba3
TebohoRomeo <teboholekhalo13@gmail.com>
2020-02-04 14:35:25 +0200

    Website

--- src/email_validtaion.js
+++ src/email_validtaion.js
@@ -1,16 +1,1 @@
-// document.getElementsByClassName('form')
-
-// function validateEmail(email) {
-//     let reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//     return reg.test(String(email).toLowerCase());
-// }
-
-// console.log(validateEmail('tebohol.com'))
-
-// function validateForm() {
-//     var x = document.forms["myForm"]["fname"].value;
-//     if (x == "") {
-//       alert("Name must be filled out");
-//       return false;
-//     }
-//   }
+// Send Email
